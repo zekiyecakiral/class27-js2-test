b9210caabc4b6e2ada805fccf2fb91508fa3b1e6
Noer Paanakker <noer@hackyourfuture.net>
2020-05-04 09:07:14 +0200

    added test files + readme

---
+++ README.md
@@ -0,0 +1,34 @@
+# JavaScript1 and JavaScript2 Test
+
+This test will be about the most important JavaScript concepts you’ve learned about so far (JavaScript1 and JavaScript2).
+
+## Test duration
+
+- 1 hour
+
+## How to start
+
+1. Create a `fork` of this repository
+2. Clone your `forked repository` to your machine
+3. Navigate to the folder `class27-js2-test`
+4. Get started with writing code!
+
+## The requirements
+
+- Write clean code (remove unnecessary `console.log`s, format correctly, etc.)
+- You are not allowed to modify the `index.html` file
+
+## How to submit your test
+
+1. Push your code to your `forked repository`
+2. Create a new pull request to the original repository
+
+## Tips for success!
+
+- **Look for low hanging fruit**. Which of the assignments is easiest for you to do? Tackle that one first.
+- Make sure that all **syntax is correct** when writing code on paper. For instance: don’t forget brackets or semicolons.
+- **If you get stuck in an assignment, move on to the next one**. You can always come back later if time permits.
+- After finishing an assignment, read the question again to **make sure you actually gave an answer to the question**.
+- Before handing in the test, read it all over again to pick out the small mistakes. **Double check your work!**
+
+Good luck!
---
+++ js2-test.js
@@ -0,0 +1,81 @@
+// Name:
+// Class:
+
+// JavaScript1
+
+/* 1. Create a function, named logObj, that:
+    - Takes 1 argument: an object, with 3 properties
+    - Loops over the object and put each value into a separate variable
+    - Concatenates the values into a single string
+    - Log to the console the string, including the following: '{NAME} is {AGE} and works as an {JOB}!'
+    - Return the string
+
+    Expected output:
+    'Noer is 28 and works as an Education Director'
+*/
+
+/* 2. Create a function, named logNumbers, that:
+      - Takes 2 arguments: a start number and an end number
+      - Has a loop that starts from the start number and ends until the end number
+      - Log to the console each number, however...
+      - If the number is a multiple of 3 output the string “Fizz”,
+      - If the number is a multiple of 5 output the string “Buzz”
+      - If the number is a multiple of 3 AND 5 output the string “FizzBuzz”
+      - Make use of the modulo operator and check for the remainder: %
+
+      Expected output:
+      logNumbers(1, 15);
+      1
+      2
+      Fizz
+      4
+      Buzz
+      6
+      ...
+      13
+      14
+      FizzBuzz
+
+      Use the following values: 1 (start number) and 100 (end number)
+*/
+
+// JavaScript2
+
+/* 3. Create a function, named toNumbers, that:
+      - Takes in 1 argument: an array of strings
+      - Get the index position number of each value
+      - Create a new array that includes all these numbers 
+      - Make use of the map() function
+      - Log to the console the array of numbers
+      - Return the array of numbers
+
+      Expected output:
+      [1, 2, 3, 4]
+
+      Use the following array: const letters = ['a', 'b', 'c', 'd'];
+*/
+
+/* 4. Create a function, called injectBooksToDOM, that:
+      - Takes 1 argument: an array of objects
+      - Loops over the array
+      - Injects each object's content into the DOM in an unordered list
+      - Make use of the forEach() function
+      - Returns true if it works, false if it doesn't
+  
+      Target the '#root' element in 'index.html'.
+      Use the following object: 
+      const books = [
+            {
+                  bookName: "The Nature of Software Development",
+                  author: "Ron Jeffries",
+                  coverURL:
+                        "https://cdn-images-1.medium.com/max/1200/1*CQRh-pFTZ97ReXogbefleQ.png"
+            },
+            {
+                  bookName: "Clean Code",
+                  author: "Robert Cecil Martin",
+                  coverURL:
+                        "https://images-na.ssl-images-amazon.com/images/I/515iEcDr1GL._SX258_BO1,204,203,200_.jpg"
+            }
+      ];
+*/
