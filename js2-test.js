5406d50733ffd688a64c41ac04a67cb8da740da5
Tjebbee <33282467+Tjebbee@users.noreply.github.com>
2020-05-07 09:56:22 +0200

    Update js2-test.js

--- js2-test.js
+++ js2-test.js
@@ -79,3 +79,28 @@
             }
       ];
 */
+
+/*
+5. What's the output for this snippet? Is the output deterministic (always the same) or might it depend on the browser
+we run this in? 
+Explain your answer in less than 150 words. (The 'yes or no' answer doesn't really matter to us, 
+the explanation why is the important part)
+
+function test() {
+    console.log('one');
+    setTimeout(function() {
+        console.log('two');
+        setTimeout(function() {
+            console.log('three');
+        }, 0);
+    }, 1000);
+    setTimeout(function() {
+        console.log('four');
+        setTimeout(function() {
+            console.log('five');
+        }, 1000);
+    }, 0);
+    console.log('six');
+}
+test(); // ?
+*/
